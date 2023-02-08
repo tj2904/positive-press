47362e42269c207affa5647a250c06a46705574e
tj2904 <tj2904+github@gmail.com>
2023-02-08 22:28:53 +0000

    adds deta to default-src

--- next.config.js
+++ next.config.js
@@ -17,14 +17,14 @@ const nextConfig = {
 }
 
 const ContentSecurityPolicy = `
-    default-src 'self' vercel.live;
+    default-src 'self' https://i3g7qv.deta.dev;
     script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
     style-src 'self' 'unsafe-inline';
     img-src * blob: data:;
     media-src 'none';
     connect-src *;
     font-src 'self';
-`;
+`
 
 const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
