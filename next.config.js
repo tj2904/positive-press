739b0596d245773018b5e59b15ccc47cd1831adf
tj2904 <tj2904+github@gmail.com>
2023-02-08 23:05:53 +0000

    update security headers

--- next.config.js
+++ next.config.js
@@ -14,6 +14,15 @@ const nextConfig = {
       },
     ],
   },
+  
+  headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
 
 const ContentSecurityPolicy = `
