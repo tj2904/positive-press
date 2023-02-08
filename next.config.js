b7694f3b80ec76899c8b7e173cfced920f5ac8f5
tj2904 <tj2904+github@gmail.com>
2023-02-08 22:20:03 +0000

    update csp

--- next.config.js
+++ next.config.js
@@ -16,4 +16,22 @@ const nextConfig = {
   },
 }
 
+const ContentSecurityPolicy = `
+    default-src 'self' vercel.live;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
+    style-src 'self' 'unsafe-inline';
+    img-src * blob: data:;
+    media-src 'none';
+    connect-src *;
+    font-src 'self';
+`;
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+];
+
 module.exports = nextConfig
