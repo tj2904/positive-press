639763af067e216470a059f667ededfd16233f45
tj2904 <tj2904+github@gmail.com>
2023-02-11 00:06:11 +0000

    layout improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
       <head />
       <body className="bg-neutral-50">
         <Header />
-        <div className="container mx-auto px-4 mt-7">
+        <div className="container mx-auto max-w-8xl px-4 sm:px-6 lg:px-8 mt-7">
           {/* <div className="mt-16 text-transparent text-6xl bg-clip-text bg-gradient-to-r from-blue-700 to-teal-300 text-center font-serif font-extrabold">
             Positive Press
           </div> */}
