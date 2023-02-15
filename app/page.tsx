692bf6d31c9be8face23d85fab1f643f05ebf2fe
tj2904 <tj2904+github@gmail.com>
2023-02-15 12:06:23 +0000

    add styling to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 function Home() {
 
-return (
-  <>
-    <h1 className="text-6xl p-4 ">Home Page</h1>
-<p>Details coming soon...</p>
+  return (
+    <>
+      <h1 className="text-3xl font-extrabold tracking-tight text-gray-700 sm:text-4xl"
+      >Home Page</h1>
+      <p className="italic text-gray-500 mt-0 mb-4">Details coming soon...</p>
 
-  </>
+    </>
 
-)
+  )
 }
 
 export default Home
