a52fc561a83754a58b14ce1681aa4ce7d6de1851
tj2904 <tj2904+github@gmail.com>
2023-02-22 10:06:26 +0000

    loading page updates

--- app/trial/loading/page.tsx
+++ app/trial/loading/page.tsx
@@ -11,7 +11,7 @@ function page() {
                 <ArticleSkelton />
                 <ArticleSkelton />
                 <ArticleSkelton />
-
+                
             </div>
         </>
     )
