c283a01df280b0d855ef30ef8ab01a181c1a8d8d
tj2904 <tj2904+github@gmail.com>
2023-03-01 15:14:50 +0000

    re-commited

--- app/trial/page.tsx
+++ app/trial/page.tsx
@@ -28,6 +28,10 @@ export default async function Page() {
       return -1
     }
   })
+  const news = sortedNews.slice(0, 1)
+  const singleNews = news.map((article: NewsResponse) => {
+    return <FrontPageLargeArticle {...article} />
+  })
 
   const topNewsData: any = await getTopNewsData()
   const sortedTopNews = topNewsData.data[0].sort(
@@ -37,14 +41,6 @@ export default async function Page() {
       }
     },
   )
-
-
-
-  const news = sortedNews.slice(0, 1)
-
-  const singleNews = news.map((article: NewsResponse) => {
-    return <FrontPageLargeArticle {...article} />
-  })
 
   const top5News = sortedTopNews.slice(0, 5)
 
