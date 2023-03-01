402fbb1e44ae14e4ce8259fa4cd597a72f849871
tj2904 <tj2904+github@gmail.com>
2023-03-01 15:14:29 +0000

    none parallel fetching

--- app/trial/page.tsx
+++ app/trial/page.tsx
@@ -1,57 +1,71 @@
-
 import FrontPageLargeArticle from "../components/FrontPageLargeArticle"
 import TopNewsHeadlineList from "../components/TopNewsHeadlineList"
 
 // This is a trial page to test the API and other things
 async function getNewsData() {
-    const res = await fetch('https://i3g7qv.deta.dev/api/v1/vader/live/england', { cache: 'no-store' },)
-    return res.json().catch((error) => { console.error("getData Error: ", error) })
+  const res = await fetch("https://i3g7qv.deta.dev/api/v1/vader/live/england", {
+    cache: "no-store",
+  })
+  return res.json().catch((error) => {
+    console.error("getData Error: ", error)
+  })
 }
 
-async function getTop5NewsData() {
-    const res = await fetch('https://i3g7qv.deta.dev/api/v1/vader/summary/pos/top', { next: { revalidate: 600 } })
-    return res.json().catch((error) => { console.error("getTopNewsData Error: ", error) })
+async function getTopNewsData() {
+  const res = await fetch(
+    "https://i3g7qv.deta.dev/api/v1/vader/summary/pos/top",
+    { next: { revalidate: 600 } },
+  )
+  return res.json().catch((error) => {
+    console.error("getData Error: ", error)
+  })
 }
 
 export default async function Page() {
-    // Initiate both requests in parallel
-    const getData = getNewsData();
-    const getTopNewsData = getTop5NewsData();
-    // Wait for the promises to resolve
-    const [data, topNewsData] = await Promise.all([getData, getTopNewsData]);
+  const liveNews: any = await getNewsData()
+  const sortedNews = liveNews.sort((a: NewsResponse, b: NewsResponse) => {
+    if (a.vaderSummary.compound > b.vaderSummary.compound) {
+      return -1
+    }
+  })
+
+  const topNewsData: any = await getTopNewsData()
+  const sortedTopNews = topNewsData.data[0].sort(
+    (a: NewsResponse, b: NewsResponse) => {
+      if (a.vaderSummary.compound > b.vaderSummary.compound) {
+        return -1
+      }
+    },
+  )
 
 
-    const sortedNews = data.sort((a: NewsResponse, b: NewsResponse) => {
-        if (a.vaderSummary.compound > b.vaderSummary.compound) {
-            return -1
-        }
-    })
 
-    const news = sortedNews.slice(0, 1)
+  const news = sortedNews.slice(0, 1)
 
-    const singleNews = news.map((article: NewsResponse) => {
-        return <FrontPageLargeArticle {...article} />
-    })
+  const singleNews = news.map((article: NewsResponse) => {
+    return <FrontPageLargeArticle {...article} />
+  })
 
-    const sortedTopNews = topNewsData.data[0].sort((a: NewsResponse, b: NewsResponse) => {
-        if (a.vaderSummary.compound > b.vaderSummary.compound) {
-            return -1
-        }
-    })
+  const top5News = sortedTopNews.slice(0, 5)
 
-    const top5News = sortedTopNews.slice(0, 5)
-
-    return (
-        <main className="mb-14">
-            <h1 className="text-3xl font-extrabold tracking-tight text-gray-700 sm:text-4xl">Top Positive News</h1>
-            <p className="italic text-gray-500 mt-0 mb-4">The most recent article from England, and the top 5 most positive news stories since the site started.</p>
-            <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-4">
-                <div className="md:col-span-2 ">{singleNews}</div>
-                <div className=" md:col-span-1 bg-neutral-100 rounded-md p-4">
-                    <h3 className="text-xl font-extrabold tracking-tight text-gray-700 text-center pb-2">Top 5 All-Time Positive Headlines</h3>
-                    <TopNewsHeadlineList data={top5News} />
-                </div>
-            </div>
-        </main>
-    )
+  return (
+    <main className="mb-14">
+      <h1 className="text-3xl font-extrabold tracking-tight text-gray-700 sm:text-4xl">
+        Top Positive News
+      </h1>
+      <p className="italic text-gray-500 mt-0 mb-4">
+        The most recent article from England, and the top 5 most positive news
+        stories since the site started.
+      </p>
+      <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="md:col-span-2 ">{singleNews}</div>
+        <div className=" md:col-span-1 bg-neutral-100 rounded-md p-4">
+          <h3 className="text-xl font-extrabold tracking-tight text-gray-700 text-center pb-2">
+            Top 5 All-Time Positive Headlines
+          </h3>
+          <TopNewsHeadlineList data={top5News} />
+        </div>
+      </div>
+    </main>
+  )
 }
