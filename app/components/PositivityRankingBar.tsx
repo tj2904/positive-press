1cebf65f550e50e629b57b31c82aae6abe3dd4ff
tj2904 <tj2904+github@gmail.com>
2023-04-24 11:59:23 +0100

    Add colour tests to ranking bar

--- app/components/PositivityRankingBar.tsx
+++ app/components/PositivityRankingBar.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
+import { percentage } from '../utils/formatting';
 
 export default function PositivityRankingBar(props: any) {
 
-  let percentage = (decimal: number) => {
-    return `${(decimal * 100).toFixed(1)}%`;
-  };
+
 
   return (
     <>
       {props.vaderSummaryCompound > 0 && (<div className="text-slate-600 text-sm bg-gray-100 rounded-r-md">
         <div className=" bg-gradient-to-l from-green-600 rounded-md" style={{ width: props.rating }}>
-          {percentage(props.vaderSummaryCompound)}
+          {percentage(props.vaderSummaryCompound, 1)}
         </div>
       </div>)}
 
@@ -18,7 +17,7 @@ export default function PositivityRankingBar(props: any) {
 
       {props.vaderSummaryCompound < 0 && (<div className="text-slate-600 text-sm bg-gray-100 rounded-r-md">
         <div className="bg-gradient-to-l from-red-600 rounded-md" style={{ width: props.negRating }}>
-          {percentage(props.vaderSummaryCompound)}
+          {percentage(props.vaderSummaryCompound, 1)}
         </div>
       </div>)}
     </>
