4e7e0b053d49e780df082a90e5e37c3197237128
tj2904 <tj2904+github@gmail.com>
2023-04-24 12:02:26 +0100

    adds test for negative value

--- __tests__/formatting.test.tsx
+++ __tests__/formatting.test.tsx
@@ -12,3 +12,7 @@ it("percentage formatting works to 2 dp", () => {
 it("percentage formatting works with passed dp value", () => {
   expect(percentage("0.123456789", 5)).toEqual("12.34568%")
 })
+
+it("percentage formatting works with neagtive values", () => {
+  expect(percentage("-0.1234")).toEqual("-12%")
+})
