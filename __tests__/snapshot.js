6d16ed82ac2a86daf4f590a77e06537b390b91d5
tj2904 <tj2904+github@gmail.com>
2023-04-24 11:59:23 +0100

    Add colour tests to ranking bar

--- __tests__/snapshot.js
+++ __tests__/snapshot.js
@@ -1,7 +1,18 @@
 import { render } from "@testing-library/react"
 import Header from "../app/components/Header"
+import PositivityRankingBar from "../app/components/PositivityRankingBar"
 
 it("renders nav bar unchanged", () => {
   const { container } = render(<Header />)
   expect(container).toMatchSnapshot()
+})
+
+it("renders PostivityRankingBar with green", () => {
+const { greenBar } = render(<PositivityRankingBar props={0.88} />)
+expect(greenBar).toMatchSnapshot()
+})
+
+it("renders PostivityRankingBar with red", () => {
+  const { redBar } = render(<PositivityRankingBar props={-0.88} />)
+  expect(redBar).toMatchSnapshot()
 })
