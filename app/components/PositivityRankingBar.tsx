b25b50e6a1bce65ab72ab10a750b2ba8728540d9
tj2904 <tj2904+github@gmail.com>
2023-04-24 13:20:37 +0100

    Adds article test and snapshot

--- app/components/PositivityRankingBar.tsx
+++ app/components/PositivityRankingBar.tsx
@@ -6,7 +6,7 @@ export default function PositivityRankingBar(props: any) {
 
 
   return (
-    <>
+    <div data-testid="article-rating">
       {props.vaderSummaryCompound > 0 && (<div className="text-slate-600 text-sm bg-gray-100 rounded-r-md">
         <div className=" bg-gradient-to-l from-green-600 rounded-md" style={{ width: props.rating }}>
           {percentage(props.vaderSummaryCompound, 1)}
@@ -20,6 +20,6 @@ export default function PositivityRankingBar(props: any) {
           {percentage(props.vaderSummaryCompound, 1)}
         </div>
       </div>)}
-    </>
+    </div>
   )
 }
