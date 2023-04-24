19bd9bd415555562facec41a7047ddaaeea3b3b7
tj2904 <tj2904+github@gmail.com>
2023-04-24 23:13:34 +0100

    add some metadata to the head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Positive Press - News ranked for positivity</title>
+      <meta
+        name="description"
+        content="Ranking and sorting news to find the positive."
+      />
+      <link rel="icon" href="/public/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
