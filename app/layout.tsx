7020ecf88dd50305b5399ee4012dc75a8e7726c4
tj2904 <tj2904+github@gmail.com>
2023-04-24 15:20:52 +0100

    Adds landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,14 +13,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <head />
-      <body className="bg-neutral-50">
-        <Header />
-        <div className="container mx-auto max-w-8xl px-4 sm:px-6 lg:px-8 mt-7">
-          {/* <div className="mt-16 text-transparent text-6xl bg-clip-text bg-gradient-to-r from-blue-700 to-teal-300 text-center font-serif font-extrabold">
-            Positive Press
-          </div> */}
+      <body>
+        
+        
           {children}
-        </div>
+        
         <AnalyticsWrapper />
       </body>
     </html>
