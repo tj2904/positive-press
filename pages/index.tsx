7255d5773a4a31c0f5b2362ce555e3b6d825a629
Tim <tj2904@users.noreply.github.com>
2023-04-25 09:39:54 +0100

    Fix typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           <div className="mt-24 sm:mt-32 lg:mt-16">
             <div className="inline-flex space-x-6">
               <span className="rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-400 ring-1 ring-inset ring-indigo-500/20">
-                Comming Soon...
+                Coming Soon...
               </span>
               {/*  <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
                 <span>Just shipped v1.0</span>
