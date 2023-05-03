cf213de83b077db5df5c6def23ae153d6025e42e
tj2904 <tj2904+github@gmail.com>
2023-05-03 13:15:47 +0100

    add landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import "../styles/globals.css";
-import Header from "./components/Header";
 import { Inter } from '@next/font/google'
 import { AnalyticsWrapper } from './components/analytics';
 
@@ -14,13 +13,9 @@ export default function RootLayout({
     <html lang="en" className={inter.className}>
       <head />
       <body className="bg-neutral-50">
-        <Header />
-        <div className="container mx-auto max-w-8xl px-4 sm:px-6 lg:px-8 mt-7">
-          {/* <div className="mt-16 text-transparent text-6xl bg-clip-text bg-gradient-to-r from-blue-700 to-teal-300 text-center font-serif font-extrabold">
-            Positive Press
-          </div> */}
+       
           {children}
-        </div>
+        
         <AnalyticsWrapper />
       </body>
     </html>
