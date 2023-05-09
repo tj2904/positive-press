2b7536acd4fbf0d79186de52b6cf880e67bc50c1
tj2904 <tj2904+github@gmail.com>
2023-05-09 13:39:18 +0100

    testing image dimension

--- app/head.tsx
+++ app/head.tsx
@@ -8,7 +8,8 @@ export default function Head() {
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Positive Press" />
       <meta property="og:description" content="Sorting live news to find the positive. We analyse news stories to find and sort the up-beat, heart-warming stories." />
-      <meta property="og:image" content="/images/logo-512x512.png" />
+      {/* <meta property="og:image" content="/images/logo-512x512.png" /> */}
+      <meta property="og:image" content="/images/logo-32x32.png" />
     </>
   )
 }
