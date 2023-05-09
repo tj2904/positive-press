e2f2dc850af40ae147cee0412cd0a0289f844106
Tim <tj2904@users.noreply.github.com>
2023-05-09 15:05:26 +0100

    Add open graph metadata (#14)
    * adds common tags and logo image

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,12 @@ export default function Head() {
     <>
       <title>Positive Press</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta content="Positive Press - Sorting live news to find the positive. We analyse news stories to sort and filter the up-beat, heart-warming stories." name="description" />
+      <meta content="Positive Press - Sorting live news to find the positive. We analyse news stories to find and sort the up-beat, heart-warming stories." name="description" />
+      <meta property="og:url" content="https://positive-press.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Positive Press" />
+      <meta property="og:description" content="Sorting live news to find the positive. We analyse news stories to find and sort the up-beat, heart-warming stories." />
+      <meta property="og:image" content="/images/logo-512x512.png" />
     </>
   )
 }
