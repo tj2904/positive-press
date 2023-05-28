89c8e2367b2410e2224db17260c4dbce3a190fe0
tj2904 <tj2904+github@gmail.com>
2023-05-28 22:49:43 +0100

    reinstate pages dir

---
+++ pages/_api.tsx
@@ -0,0 +1,15 @@
+import { Inter } from "@next/font/google"
+import type { AppProps } from "next/app"
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={`${inter.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  )
+}
---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <title>Positive Press - News ranked for positivity</title>
+      <meta
+        name="description"
+        content="Ranking and sorting news to find the positive."
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
